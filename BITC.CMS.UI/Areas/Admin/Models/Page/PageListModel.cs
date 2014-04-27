a9d4995d2f6534bfa8a6e9334d2da7f2590505b7
Trung Nguyen Duc <trungnd1985@gmail.com>
2014-04-27 22:46:14 +0700

    fixed

---
+++ BITC.CMS.UI/Areas/Admin/Models/Page/PageListModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BITC.CMS.UI.Areas.Admin.Models.Page
+{
+    public class PageListModel
+    {
+        public int PageID { get; set; }
+
+        public string PageTitle { get; set; }
+
+        public string Url { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ BITC.CMS.UI/Controllers/PageController.cs
@@ -0,0 +1,35 @@
+﻿using BITC.CMS.Data;
+using BITC.CMS.Repository;
+using BITC.Library.Data;
+using BITC.Web.Library.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BITC.CMS.UI.Controllers
+{
+    public class PageController : BitcController
+    {
+        public ActionResult Index(string url)
+        {
+            var _uow = new UnitOfWork();
+            var _repo = _uow.GetRepository<Page>();
+            if (string.IsNullOrEmpty(url))
+            {
+                url = "/";
+            }
+            var _model = _repo.SingleOrDefault(i => i.Url == url);
+
+            if (_model !=null)
+            {
+                ViewBag.Title = _model.PageTitle;
+                ViewBag.Description = _model.Description;
+                ViewBag.Keywords = _model.Keywords;
+            }
+            
+            return View(_model);
+        }
+    }
+}
