22b68790d1c8e8916462018ce4780c8fb92b4aea
Trung Nguyen Duc <trungnd1985@gmail.com>
2014-04-27 22:31:19 +0700

    Update source

---
+++ Libs/BITC.Web.Library/Mvc/BitcController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BITC.Web.Library.Mvc
+{
+    public class BitcController : Controller
+    {
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            base.OnException(filterContext);
+        }
+    }
+}
---
+++ Libs/BITC.Web.Library/PageInfo.cs
@@ -0,0 +1,30 @@
+﻿using BITC.Library.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BITC.Web.Library
+{
+
+    public class PageInfo : BaseEntity
+    {
+        public int PageID { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string PageTitle { get; set; }
+
+        [AllowHtml]
+        public string Body { get; set; }
+
+        public string Template { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Url { get; set; }
+    }
+}
