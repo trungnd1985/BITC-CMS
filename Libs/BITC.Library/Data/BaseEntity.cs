7b0b2e07bff204f933ea5b56ae94af12069b36d6
Trung Nguyen Duc <trungnd1985@gmail.com>
2014-04-27 22:47:51 +0700

    fiexed

---
+++ Libs/BITC.Library/Data/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BITC.Library.Data
+{
+    public class BaseEntity
+    {
+        public string Description { get; set; }
+
+        public int SortOrder { get; set; }
+
+        public bool Inactive { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string ModifiedBy { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public string Culture { get; set; }
+    }
+}
