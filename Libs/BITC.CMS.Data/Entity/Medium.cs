896d2c0943a2230d9835f246fc7ab9c136aae8ca
Trung Nguyen Duc <trungnd1985@gmail.com>
2014-05-21 06:55:53 +0700

    Medium

---
+++ Libs/BITC.CMS.Data/Entity/Medium.cs
@@ -0,0 +1,20 @@
+﻿using BITC.Library.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BITC.CMS.Data.Entity
+{
+    public partial class Medium : BaseEntity
+    {
+        public int MediaID { get; set; }
+
+        public int MediaType { get; set; }
+
+        public string MIMEType { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
